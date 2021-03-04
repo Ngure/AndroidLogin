d0c5823d1c17d0d981b354ebb186110af5347ea1
Chriss Ngure <chrissngure@gmail.com>
2021-03-04 12:14:43 +0300

    misc

--- app/src/main/java/com/example/androidlogin/home/HomeActivity.kt
+++ app/src/main/java/com/example/androidlogin/home/HomeActivity.kt
@@ -5,19 +5,28 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.example.androidlogin.R
 import com.example.androidlogin.settings.SettingsActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+        title = "Home"
 
         val ab = supportActionBar
         ab?.setDisplayHomeAsUpEnabled(true)
-        ab?.title = "Home"
+
+        setSupportActionBar(mainToolbar)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val value = extras.getString("key")
+            //The key argument here must match that used in the other activity
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -32,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         // Handle presses on the action bar menu items
         when (item.itemId) {
             R.id.toolbar_settings -> {
-                val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+                val intent = Intent(this@HomeActivity, SettingsActivity::class.java)
                 // start your activity
                 this.startActivity(intent)
             }
