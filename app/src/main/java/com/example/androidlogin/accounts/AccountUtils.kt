b97d517ca95ddb655b5bcfd75ef78d686cb99b37
Chriss Ngure <chrissngure@gmail.com>
2021-03-10 10:47:36 +0300

    add accounts utilities

---
+++ app/src/main/java/com/example/androidlogin/accounts/AccountUtils.kt
@@ -0,0 +1,14 @@
+package com.example.androidlogin.accounts
+
+class AccountUtils {
+    companion object {
+
+        const val SOURCE_FACEBOOK = "facebook.com"
+        const val SOURCE_GOOGLE = "google.com"
+
+        const val LOGIN_RESULT_CODE_OK = 1
+        const val LOGIN_RESULT_CODE_PROFILES_FAILED = 2
+        const val LOGIN_RESULT_CODE_ACCOUNT_ALREADY_EXISTS = 3
+        const val LOGIN_RESULT_CODE_INVALID_EMAIL = 4
+    }
+}
